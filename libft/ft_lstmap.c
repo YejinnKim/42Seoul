ac8236ed5f34a4ba5f56126d20500be11ff4e856
yejinkim <kyj1860@naver.com>
2022-08-04 15:35:45 +0900

    libft

---
+++ libft/ft_lstmap.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yejinkim <yejinkim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/13 16:41:33 by yejinkim          #+#    #+#             */
+/*   Updated: 2022/07/18 15:25:00 by yejinkim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_lst;
+	t_list	*tmp;
+
+	if (!lst || !f)
+		return (0);
+	new_lst = NULL;
+	while (lst)
+	{
+		tmp = ft_lstnew(f(lst->content));
+		if (!tmp)
+		{
+			ft_lstclear(&new_lst, del);
+			return (0);
+		}
+		ft_lstadd_back(&new_lst, tmp);
+		tmp = tmp->next;
+		lst = lst->next;
+	}
+	return (new_lst);
+}
