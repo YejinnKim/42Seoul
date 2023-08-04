5e0fa1d39e2c0f9cd5c28b8aae60eda1e5520a0f
yejinkim <kyj1860@naver.com>
2023-08-04 15:33:40 +0900

    cpp03 ex00 8/4

--- cpp_module_03/ex00/main.cpp
+++ cpp_module_03/ex00/main.cpp
@@ -5,23 +5,31 @@ int main()
 	ClapTrap	a("A");
 	ClapTrap	b("B");
 	
+	a.printStatus();
+	b.printStatus();
 	std::cout << std::endl;
+
 	a.attack("B");
 	b.takeDamage(a.getAttackDamage());
 	a.printStatus();
 	b.printStatus();
 	std::cout << std::endl;
 
-	a.beRepaired(3);
+	a.beRepaired(10);
 	b.beRepaired(5);
 	a.printStatus();
 	b.printStatus();
 	std::cout << std::endl;
 
-	while (a.getEnergyPoints() > 0)
-		a.beRepaired(5);
+	a.takeDamage(100);
 	a.printStatus();
-	a.beRepaired(5);
+	a.attack("B");
+	std::cout << std::endl;
+
+	while (b.getEnergyPoints() > 0)
+		b.beRepaired(5);
+	b.printStatus();
+	b.beRepaired(5);
 	std::cout << std::endl;
 
 	return 0;
