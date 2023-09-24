8b5d779d6d999f10eccaf52b081b7097114fad5e
yejinkim <kyj1860@naver.com>
2023-09-24 17:55:24 +0900

    cpp08 ex00 9/24

---
+++ cpp_module_08/ex00/main.cpp
@@ -0,0 +1,15 @@
+#include "easyfind.hpp"
+#include <vector>
+
+int main()
+{
+	std::vector<int> v;
+
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+
+	std::cout << *easyfind(v, 2) << std::endl;
+
+	return 0;
+}
