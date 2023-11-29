7ad230f92b3efeeb309d8373d250990001a23d37
yejinnkim <kyj1860@naver.com>
2023-11-29 10:34:08 +0900

    inception wordpress config

--- inception/srcs/requirements/nginx/tools/entrypoint.sh
+++ inception/srcs/requirements/nginx/tools/entrypoint.sh
@@ -5,6 +5,6 @@ set -e
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
 	-keyout /etc/ssl/private/nginx_pkey.pem \
 	-out /etc/ssl/certs/nginx.crt \
-	-subj "/C=KR/L=Seoul/O=42seoul/CN=yejinkim.42.kr"
+	-subj "/C=KR/ST=Seoul/L=Gangnam/O=42seoul/CN=yejinkim.42.kr"
 
 exec "$@"
--- inception/srcs/requirements/wordpress/tools/entrypoint.sh
+++ inception/srcs/requirements/wordpress/tools/entrypoint.sh
@@ -3,7 +3,12 @@
 set -e
 
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 0.0.0.0:9000/g' /etc/php/7.3/fpm/pool.d/www.conf
-sed -i 's/;clear_env = no/clear_env = no/g' /etc/php/7.3/fpm/pool.d/www.conf
 mkdir -p /run/php
+
+wp core download --path=/var/www/html --allow-root
+wp core config --path=/var/www/html --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$WORDPRESS_DB_HOST --allow-root
+wp core install --path=/var/www/html --url=https://$DOMAIN_NAME --title=inception \
+	--admin_user=$WORDPRESS_ADMIN --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email --allow-root
+wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASS --allow-root
 
 exec "$@"
